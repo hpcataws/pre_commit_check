d2413e07add68d9c653114d5d725d77a0fe5484c
Thorsten Schütt <schuett@gmail.com>
2022-07-16 14:40:50 +0200

    improve test coverage

--- pre_commit_check/test_git.py
+++ pre_commit_check/test_git.py
@@ -1,5 +1,5 @@
 #import pytest
-from pre_commit_check.git import get_remote_url, is_github_repo, is_default_branch_main
+from pre_commit_check.git import get_remote_url, is_github_repo, is_default_branch_main, is_aws_codecommit_repo
 
 
 def test_git_remote_url():
@@ -12,3 +12,7 @@ def test_is_github_repo():
 
 def test_is_default_branch_main():
     assert is_default_branch_main() is True
+
+
+def test_is_aws_codecommit_repo():
+    assert is_aws_codecommit_repo() is False
